2f6734fdf28672274cbbe138832456bea7ab0314
Juanma Serrano <joimpoke@gmail.com>
2020-05-01 20:18:44 +0900

    Add configuration management of sorts

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,15 @@ import { Game } from "./game";
 import { Deck } from './deck';
 import { FisherYates } from './shuffle/fisher-yates';
 import { Card } from './card';
+import fs from 'fs';
+
+if (!fs.existsSync('./config.json')) {
+    console.error('Please, copy the file config-example.json into config.json and fill the configuration values.');
+    process.exit(-1);
+}
 
 let cards = new Array<Card>();
 let deck = new Deck(cards, new FisherYates());
 let game = new Game(deck);
+
 // Here goes the discord client (or any client because decoupling is magic!)
