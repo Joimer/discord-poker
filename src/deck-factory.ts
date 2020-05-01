e4874547f5f2abd870c5530ff53c1c1684684653
Juanma Serrano <joimpoke@gmail.com>
2020-05-02 01:04:10 +0900

    Improve deck factory code :)

--- src/deck-factory.ts
+++ src/deck-factory.ts
@@ -5,16 +5,11 @@ import { CardSuit } from './card-suit';
 
 export class DeckFactory {
     static getPokerDeck(): Deck {
-        let suits = new Array<CardSuit>();
-        suits.push(CardSuit.CLOVER);
-        suits.push(CardSuit.DIAMOND);
-        suits.push(CardSuit.HEART);
-        suits.push(CardSuit.SPADES);
+        let suits = [CardSuit.CLOVER, CardSuit.DIAMOND, CardSuit.HEART, CardSuit.SPADES];
         let cards = new Array<Card>();
         let discriminators = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
         for (let suit of suits) {
-            for (let i = 0; i < 13; i++) {
-                let disc = discriminators[i];
+            for (let disc of discriminators) {
                 cards.push(new Card(suit, disc));
             }
         }
