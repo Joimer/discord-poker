329060b244d6c73268e3497feeb8c31ec3e29620
Juanma Serrano <joimpoke@gmail.com>
2020-05-02 20:13:37 +0900

    Implement join and start game commands

--- src/commands/command-list.ts
+++ src/commands/command-list.ts
@@ -1,6 +1,7 @@
 import { Command } from './command';
 import { Game } from '../game';
 import { getPokerDeck } from '../deck-factory';
+import { GuildManager } from 'discord.js';
 
 export const commands = new Map<string, Command>();
 
@@ -18,14 +19,33 @@ commands.set('create', (env): string => {
     }
 });
 
-// Join an existing game
-commands.set('join', (env, content): string => {
+function startGame(game: Game): string {
+    game.start();
     return '';
+}
+
+// Join an existing game
+commands.set('join', (env): string => {
+    let game = env.games.get(env.gameId);
+    if (!game) {
+        return "There's no game going on in this channel.";
+    }
+    game.join(env.player);
+    let res = `${env.player.name} has joined the game.`;
+    // Magic number, config?
+    if (game.players.length === 10) {
+        res += "\n" + startGame(game);
+    }
+    return res;
 });
 
 // Start a game once ready
-commands.set('start', (env, content): string => {
-    return '';
+commands.set('start', (env): string => {
+    let game = env.games.get(env.gameId);
+    if (!game) {
+        return "There's no game going on in this channel.";
+    }
+    return startGame(game);
 });
 
 // Call the bet
@@ -44,6 +64,7 @@ commands.set('raise', (env, content): string => {
 });
 
 // Force end the game
+/* Implement permissions eventually
 commands.set('forceend', (env, content): string => {
     return '';
-});
+});*/
