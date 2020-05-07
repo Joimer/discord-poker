7a3d41049ff7c4fcf310b132015e87c94cf6bebb
Juanma Serrano <joimpoke@gmail.com>
2020-05-07 23:38:46 +0900

    Implement two pair combination check

--- src/combinations/two-pair.ts
+++ src/combinations/two-pair.ts
@@ -1,9 +1,43 @@
 import Card from '../card';
+import { orderCards, cardValue } from '../combinations';
 
-// TODO :D
+/**
+ * Having two different pairs of repeating numbers.
+ * If the number is the same it's no longer a two pair, it's a four of a kind.
+ * Check that first.
+ * @param cards 
+ */
 export default function hasTwoPair(cards: Array<Card>): boolean {
-    if (cards) {
-        return true;
+    if (cards.length < 4) {
+        return false;
     }
-    return true;
+
+    orderCards(cards);
+
+    let pairs = 0;
+    let sameCount = 1;
+    let previousValue = null;
+
+    // Wow what a monstruous thing
+    for (let card of cards) {
+        let cardVal = cardValue(card);
+        if (previousValue !== null) {
+            if (cardVal === previousValue) {
+                sameCount++;
+                if (sameCount === 2) {
+                    pairs++;
+                    if (pairs === 2) {
+                        return true;
+                    }
+                    sameCount = 0;
+                }
+            } else {
+                sameCount = 1;
+            }
+        }
+
+        previousValue = cardVal;
+    }
+
+    return false;
 }
